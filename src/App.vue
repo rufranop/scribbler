4c7152c83d6c099a70c8e6fd071489097cc5010a
Paolo Rufrano <rufranop@gmail.com>
2019-10-27 02:10:07 +0200

    project setup

--- src/App.vue
+++ src/App.vue
@@ -1,28 +1,37 @@
 <template>
-  <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
-  </div>
+    <div class="app-container">
+        <div class="note-holder">
+            <textarea v-model="note" placeholder="Go on, write something amazing...️"></textarea>
+        </div>
+        <div class="about">
+            <h1>scribbler.space <sup>beta</sup></h1>
+            <p>A simple tool to store a note offline. Made with Vue.js.</p>
+            <p>A project by <a href="https://twitter.com/paolorufrano" target="_blank">Paolo Rufrano</a></p>
+        </div>
+    </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
-
-export default {
-  name: 'app',
-  components: {
-    HelloWorld
-  }
-}
+    export default {
+        name: 'app',
+        data() {
+            return {
+                note: ''
+            }
+        },
+        watch: {
+            note(newNote) {
+                localStorage.setItem('note', newNote);
+            }
+        },
+        mounted() {
+            if (localStorage) {
+                this.note = localStorage.getItem('note');
+            }
+        }
+    }
 </script>
 
-<style>
-#app {
-  font-family: 'Avenir', Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
+<style lang="scss">
+    @import "./scss/styles.scss";
 </style>
