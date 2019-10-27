b7a4dae524154d4ff320b777667ac9c4e4cabe23
Paolo Rufrano <rufranop@gmail.com>
2019-10-27 20:40:55 +0200

    dark mode toggling

--- src/App.vue
+++ src/App.vue
@@ -1,5 +1,5 @@
 <template>
-    <div class="app-container">
+    <div class="app-container" :class="{'dark-mode': darkMode}">
         <div class="note-holder">
             <textarea v-model="note" placeholder="Go on, write something amazing...️"></textarea>
         </div>
@@ -7,6 +7,7 @@
             <h1>scribbler.space <sup>beta</sup></h1>
             <p>A simple tool to store a note offline. Made with Vue.js.</p>
             <p>A project by <a href="https://twitter.com/paolorufrano" target="_blank">Paolo Rufrano</a></p>
+            <button @click="toggleDark()">{{ darkMode ? 'Light Mode' : 'Dark Mode' }}</button>
         </div>
     </div>
 </template>
@@ -16,18 +17,36 @@
         name: 'app',
         data() {
             return {
-                note: ''
+                note: '',
+                darkMode: false
             }
         },
         watch: {
             note(newNote) {
                 localStorage.setItem('note', newNote);
+            },
+            darkMode() {
+                localStorage.setItem('darkMode', this.darkMode)
+            }
+        },
+        methods: {
+            toggleDark() {
+                this.darkMode = !this.darkMode;
             }
         },
         mounted() {
             if (localStorage) {
                 this.note = localStorage.getItem('note');
+                this.darkMode = JSON.parse(localStorage.getItem('darkMode'));
             }
+
+            if (this.note === 'null' || this.note === null) {
+                this.note = ''
+            }
+
+            // if (window.matchMedia && window.matchMedia('(prefers-color-scheme: dark)').matches) {
+            //     this.darkMode = true;
+            // }
         }
     }
 </script>
